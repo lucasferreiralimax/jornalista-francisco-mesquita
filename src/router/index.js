584a0cd5c9ac0fda54c1e1bcddcf7f54b9569933
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-02-06 23:21:12 -0800

    adjustment routes with i18n params locale

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,50 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { SUPPORT_LOCALES, setI18nLanguage, loadLocaleMessages } from '../i18n';
 import Home from '../views/Home.vue';
 
-const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home,
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
-  },
-];
+export default function setupRouter(i18n) {
+  const locale = i18n.mode === 'legacy' ? i18n.global.locale : i18n.global.locale.value;
 
-const router = createRouter({
-  history: createWebHistory(process.env.BASE_URL),
-  routes,
-});
+  const routes = [
+    {
+      path: '/:locale/',
+      name: 'Home',
+      component: Home,
+    },
+    {
+      path: '/:locale/about',
+      name: 'About',
+      component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: () => `/${locale}`,
+    },
+  ];
 
-export default router;
+  const router = createRouter({
+    history: createWebHistory(process.env.BASE_URL),
+    routes,
+  });
+
+  router.beforeEach(async (to, from, next) => {
+    const paramsLocale = to.params.locale;
+
+    // use locale if paramsLocale is not in SUPPORT_LOCALES
+    if (!SUPPORT_LOCALES.includes(paramsLocale)) {
+      return next(`/${locale}`);
+    }
+
+    // load locale messages
+    if (!i18n.global.availableLocales.includes(paramsLocale)) {
+      await loadLocaleMessages(i18n, paramsLocale);
+    }
+
+    // set i18n language
+    setI18nLanguage(i18n, paramsLocale);
+
+    return next();
+  });
+
+  return router;
+}
