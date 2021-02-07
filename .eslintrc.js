427a5a47d5778984b8f60eadc6a97024744ddc9b
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-02-06 23:17:14 -0800

    adjustment config .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,11 +8,14 @@ module.exports = {
     '@vue/airbnb',
   ],
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: '@babel/eslint-parser',
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-param-reassign': [2, {
+      'props': false
+    }],
   },
   overrides: [
     {
