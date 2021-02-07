f4615ddfff93ff01ee94585f51463764fb2b27f0
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-02-06 23:20:31 -0800

    config i18n in main.js

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import './registerServiceWorker';
-import router from './router';
 import store from './store';
+import setupRouter from './router';
+import { setupI18n } from './i18n';
+import pt from './locales/pt-BR.json';
 
-createApp(App).use(store).use(router).mount('#app');
+const i18n = setupI18n({
+  globalInjection: true,
+  legacy: false,
+  locale: 'pt-BR',
+  fallbackLocale: 'pt-BR',
+  messages: {
+    pt,
+  },
+});
+
+const router = setupRouter(i18n);
+
+createApp(App)
+  .use(i18n)
+  .use(store)
+  .use(router)
+  .mount('#app');
