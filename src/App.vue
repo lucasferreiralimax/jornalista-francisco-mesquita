61eff577e7a3585ace62e14e735dcb09d8ce6524
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-02-06 23:18:43 -0800

    adjustment styles and animation, import component TranslateSelect

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,26 @@
 <template>
   <router-view/>
+  <TranslateSelect/>
 </template>
+
+<script>
+import TranslateSelect from '@/components/TranslateSelect.vue';
+
+export default {
+  name: 'App',
+  components: { TranslateSelect },
+};
+</script>
 
 <style lang="scss">
 body {
-  padding: 0;
+  animation: 6s background_animation infinite alternate;
+  padding: 20px 0;
   margin: 0;
   display: flex;
   align-items: center;
   justify-content: center;
-  min-height: 100vh;
+  min-height: calc(100vh - 40px);
 }
 
 #app {
@@ -18,5 +29,17 @@ body {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+@keyframes background_animation {
+  0% {
+    box-shadow: inset 0 10px 250px #ff0;
+  }
+  50% {
+    box-shadow: inset 0 10px 250px rgb(0, 110, 255);
+  }
+  100% {
+    box-shadow: inset 0 10px 250px rgb(0, 165, 63);
+  }
 }
 </style>
