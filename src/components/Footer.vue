9edac92ef063e4915ba49035e039bba81ac0c0a4
lucasferreiralimax <lucasferreiralimax@gmail.com>
2021-02-09 17:37:59 -0800

    component footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="footer">
+    <i18n-t keypath="footer" tag="p" for="repo">
+      <a href="https://github.com/lucasferreiralimax/jornalista-francisco-mesquita" target="_blank">{{ $t('repo') }}</a>
+    </i18n-t>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+};
+</script>
+
+<style scoped lang="scss">
+a { color: #000; }
+</style>
