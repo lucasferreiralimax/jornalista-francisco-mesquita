4c5490574ac02880e74aaf0227a0e133b20082aa
lucasferreiralimax <lucasferreiralimax@gmail.com>
2023-01-03 04:14:08 -0300

    test(unit): validation href in footer

--- tests/unit/Footer.spec.js
+++ tests/unit/Footer.spec.js
@@ -5,5 +5,6 @@ describe('Footer.vue', () => {
   it('renders repo srting in footer', () => {
     const wrapper = shallowMount(Footer);
     expect(wrapper.text()).toMatch('repo');
+    expect(wrapper.find("a").attributes("href")).toMatch('https://github.com/lucasferreiralimax/jornalista-francisco-mesquita');
   });
 });
